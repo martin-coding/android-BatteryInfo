5b18201fd4d61ca172fd94ae2897e7c699ae0676
Martin Köhn <martin@Laptop-Martin.fritz.box>
2023-07-29 02:08:42 +0200

    UI Update (v1.6)
    Added landscape mode support and a new color palette. UI colors now change based on measured values.

--- app/src/main/java/com/martintools/batteryinfo/MainActivity.kt
+++ app/src/main/java/com/martintools/batteryinfo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.drawable.LayerDrawable
 import android.net.Uri
 import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
@@ -31,10 +32,6 @@ class MainActivity : AppCompatActivity() {
             val batteryPercentage = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
             updateProgressBar(batteryPercentage)
 
-            /*val statusValue = intent.getIntExtra(BatteryManager.EXTRA_STATUS, 0)
-            val statusImage = findViewById<ImageView>(R.id.status_power)
-            if (statusValue == 2) statusImage.visibility = View.VISIBLE else statusImage.visibility = View.INVISIBLE*/
-
             val statusValue = intent.getIntExtra(BatteryManager.EXTRA_STATUS, 0)
             findViewById<ImageView>(R.id.status_power).visibility = if (statusValue == 2) View.VISIBLE else View.INVISIBLE
 
@@ -42,6 +39,7 @@ class MainActivity : AppCompatActivity() {
             val tempValue = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0)/10
             val tempField = findViewById<TextView>(R.id.temperature)
             tempField.text = "$tempValue \u00B0C\n"
+            tempField.setTextColor(getColorByTemp(tempValue))
 
             val sourceField = findViewById<TextView>(R.id.source)
             when(intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)){
@@ -50,6 +48,7 @@ class MainActivity : AppCompatActivity() {
                 BatteryManager.BATTERY_PLUGGED_WIRELESS -> sourceField.text = "Wireless charging"
                 else -> sourceField.text = "Not Plugged"
             }
+            sourceField.setTextColor(getColorByLevel(batteryPercentage))
 
             val healthField = findViewById<TextView>(R.id.health)
             when(intent.getIntExtra(BatteryManager.EXTRA_HEALTH, 0)){
@@ -61,18 +60,76 @@ class MainActivity : AppCompatActivity() {
                 BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE -> healthField.text = "Failed"
                 else -> healthField.text = "Unknown"
             }
+            healthField.setTextColor(getColorByLevel(batteryPercentage))
 
             val technologyValue = intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY)
-            findViewById<TextView>(R.id.technology).text = technologyValue
+            val technologyField = findViewById<TextView>(R.id.technology)
+            technologyField.text = technologyValue
+            technologyField.setTextColor(getColorByLevel(batteryPercentage))
 
             val voltageValue = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0).toDouble()/1000
-            findViewById<TextView>(R.id.voltage).text = "$voltageValue V"
+            val voltageField = findViewById<TextView>(R.id.voltage)
+            voltageField.text = "$voltageValue V"
+            voltageField.setTextColor(getColorByLevel(batteryPercentage))
         }
+    }
+
+    private fun getColorByLevel(percentage: Int): Int {
+        val level0 = resources.getColor(R.color.level0, theme)
+        val level1 = resources.getColor(R.color.level1, theme)
+        val level2 = resources.getColor(R.color.level2, theme)
+        val level3 = resources.getColor(R.color.level3, theme)
+        val level4 = resources.getColor(R.color.level4, theme)
+        val level5 = resources.getColor(R.color.level5, theme)
+
+        val color = when {
+            percentage >= 86 -> level5
+            percentage >= 68 -> level4
+            percentage >= 52 -> level3
+            percentage >= 36 -> level2
+            percentage >= 20 -> level1
+            else -> level0
+        }
+        return color
+    }
+
+    private fun getColorByTemp(temperature: Int): Int {
+        val level0 = resources.getColor(R.color.cold1, theme)
+        val level1 = resources.getColor(R.color.cold0, theme)
+        val level2 = resources.getColor(R.color.cool, theme)
+        val level3 = resources.getColor(R.color.perfect, theme)
+        val level4 = resources.getColor(R.color.warm, theme)
+        val level5 = resources.getColor(R.color.hot, theme)
+
+        val color = when {
+            temperature >= 55 -> level5
+            temperature >= 35 -> level4
+            temperature >= 20 -> level3
+            temperature >= 5 -> level2
+            temperature >= -10 -> level1
+            else -> level0
+        }
+        return color
     }
 
     @SuppressLint("SetTextI18n")
     private fun updateProgressBar(progress: Int) {
-        findViewById<ProgressBar>(R.id.progress_bar).progress = progress
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+        val progressBarDrawable = progressBar.progressDrawable as LayerDrawable
+        progressBar.progress = progress
+
+        // Index of the second item in LayerDrawable that represents the color of the circle (circle.xml)
+        val circleItemIndex = 1
+
+        // Set new color based on level
+        val newColor = getColorByLevel(progress)
+
+        // Change the color of the circle
+        val circleShape = progressBarDrawable.getDrawable(circleItemIndex)
+        circleShape.setTint(newColor)
+        progressBar.progressDrawable = progressBarDrawable
+
+        // Update text inside of circle
         findViewById<TextView>(R.id.text_view_progress).text = "$progress%"
     }
 
